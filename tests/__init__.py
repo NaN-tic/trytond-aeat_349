bef757219086350a043f1e2fe464da74e4e5a28e
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-07-22 12:52:19 +0200

    Fix and cleanup tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,6 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 
-from .test_aeat_348 import suite
+from .test_aeat_349 import suite
 
 __all__ = ['suite']
