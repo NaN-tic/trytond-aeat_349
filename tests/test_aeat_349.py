684fe741960228b2a55b7a4df6da92db054ece92
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-11-04 15:24:57 +0100

    Migrate to version 3.4

--- tests/test_aeat_349.py
+++ tests/test_aeat_349.py
@@ -4,27 +4,22 @@
 import unittest
 import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends, doctest_dropdb
+from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import doctest_setup, doctest_teardown
 
 
 class Aeat349TestCase(unittest.TestCase):
-    '''
-    Test AEAT 349 module.
-    '''
+    'Test AEAT 349 module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('aeat_349')
 
     def test0005views(self):
-        '''
-        Test views.
-        '''
+        'Test views'
         test_view('aeat_349')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
@@ -33,10 +28,10 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
             Aeat349TestCase))
     suite.addTests(doctest.DocFileSuite('scenario_aeat349.rst',
-            setUp=doctest_dropdb, tearDown=doctest_dropdb, encoding='utf-8',
+            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     suite.addTests(doctest.DocFileSuite(
             'scenario_aeat349_alternate_currency.rst',
-            setUp=doctest_dropdb, tearDown=doctest_dropdb, encoding='utf-8',
+            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
