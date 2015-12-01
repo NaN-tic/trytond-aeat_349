de2e1ae2910d8cdae226c81997b155f419fe34a1
resteve <resteve@zikzakmedia.com>
2015-12-01 11:47:58 +0100

    Tests Scenario

--- tests/test_aeat_349.py
+++ tests/test_aeat_349.py
@@ -2,8 +2,10 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
+import doctest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
+from trytond.tests.test_tryton import doctest_setup, doctest_teardown
 
 
 class Aeat349TestCase(ModuleTestCase):
@@ -15,4 +17,11 @@ def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         Aeat349TestCase))
+    suite.addTests(doctest.DocFileSuite('scenario_aeat349.rst',
+            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
+    suite.addTests(doctest.DocFileSuite(
+            'scenario_aeat349_alternate_currency.rst',
+            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
