c605f057c7162cf9cc27bb4aabd17902b7cd8ff5
Jordi Esteve <jesteve@zikzakmedia.com>
2016-02-27 13:13:32 +0100

    Improve views and rec_name of AEAT349 type

--- invoice.py
+++ invoice.py
@@ -22,7 +22,6 @@ class Type(ModelSQL, ModelView):
     Keys types for AEAT 349 Report
     """
     __name__ = 'aeat.349.type'
-    _rec_name = 'operation_key'
 
     operation_key = fields.Selection(OPERATION_KEY, 'Operation key',
         required=True)
@@ -35,6 +34,13 @@ class Type(ModelSQL, ModelView):
             ('operation_key_uniq', Unique(t, t.operation_key),
                 'Operation key must be unique.')
             ]
+
+    def get_rec_name(self, name):
+        opts = self.fields_get('operation_key')['operation_key']['selection']
+        for key, value in opts:
+            if self.operation_key == key:
+                return value
+        return self.operation_key
 
 
 class TypeTax(ModelSQL):
