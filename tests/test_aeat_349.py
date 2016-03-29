5903bca8cfebbe5b2ade6bd7e22338a1df2fb95c
resteve <resteve@zikzakmedia.com>
2016-03-29 23:49:02 +0200

    Reactive RST tests scenarios

--- tests/test_aeat_349.py
+++ tests/test_aeat_349.py
@@ -6,6 +6,7 @@ import doctest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 
 
 class Aeat349TestCase(ModuleTestCase):
@@ -19,9 +20,11 @@ def suite():
         Aeat349TestCase))
     suite.addTests(doctest.DocFileSuite('scenario_aeat349.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     suite.addTests(doctest.DocFileSuite(
             'scenario_aeat349_alternate_currency.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
