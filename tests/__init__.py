e2f5761d47abce232c9a9f91844b710389fff0e2
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-26 13:10:59 +0200

    Replace test setuptools command by unittest discover

--- tests/__init__.py
+++
@@ -1,8 +0,0 @@
-# The COPYRIGHT file at the top level of this repository contains the full
-# copyright notices and license terms.
-try:
-    from trytond.modules.aeat_349.tests.test_aeat_349 import suite
-except ImportError:
-    from .test_aeat_349 import suite
-
-__all__ = ['suite']
---
+++ tests/test_module.py
@@ -0,0 +1,14 @@
+
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+
+from trytond.modules.company.tests import CompanyTestMixin
+from trytond.tests.test_tryton import ModuleTestCase
+
+
+class Aeat349TestCase(CompanyTestMixin, ModuleTestCase):
+    'Test Aeat349 module'
+    module = 'aeat_349'
+
+
+del ModuleTestCase
