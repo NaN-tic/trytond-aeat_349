6d9287112771fc861570c2075bfdb8683b1efb77
Raimon Esteve <raimonesteve@gmail.com>
2024-03-04 11:23:56 +0100

    Raise UnicodeEncodeError in case encode iso-8859-1 #165203

--- aeat.py
+++ aeat.py
@@ -92,6 +92,12 @@ def remove_accents(unicode_string):
     # It converts nfd to nfc to allow unicode.decode()
     return unicodedata.normalize('NFC', unicode_string_nfd)
 
+def encode_iso8859(value):
+    try:
+        return value.encode('iso-8859-1')
+    except UnicodeEncodeError:
+        raise UserError( gettext('aeat_349.msg_invalid_encode'))
+
 
 class Report(Workflow, ModelSQL, ModelView):
     """
@@ -510,7 +516,7 @@ class Report(Workflow, ModelSQL, ModelView):
             raise UserError(str(e))
         data = remove_accents(data).upper()
         if isinstance(data, str):
-            data = data.encode('iso-8859-1')
+            data = encode_iso8859(data)
         self.file_ = self.__class__.file_.cast(data)
         self.save()
 
